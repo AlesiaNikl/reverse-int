22ef6930733d44bc695e975e400c9c0e55ae8afe
Alesia Nikalayeva <alesya.meow@gmail.com>
2021-08-23 22:31:24 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0){
+    n = n * (-1);
+  }  
+  let str = n.toString();
+  let arr = str.split('');
+  str = arr.reverse().join('');
+  let result = Number(str);
+  return result; 
 }
+
